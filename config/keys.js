04b3a5fb6fe095a8740995a52697a8803920c90f
Miniar JABRI <123066240+miniarjabri@users.noreply.github.com>
2025-02-20 16:43:56 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-const username = "TER"; // Ton utilisateur MongoDB
-const password = encodeURIComponent("TERoauth"); // Encode pour éviter les erreurs
-const dbName = "todolist"; // Assure-toi que c'est bien le nom de ta base de données
+const username = "TER"; 
+const password = encodeURIComponent("TERoauth");
+const dbName = "todolist"; 
 
 module.exports = {
   MongoURI: `mongodb+srv://${username}:${password}@cluster0.p3e6b.mongodb.net/${dbName}?retryWrites=true&w=majority`,
